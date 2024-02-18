335af0fc5999e77a76f75de7554bb9dd792ef56e
Billy <zygboom@163.com>
2024-02-18 22:44:56 +0800

    update

--- src/tts-openai.tsx
+++ src/tts-openai.tsx
@@ -2,7 +2,8 @@ import { Clipboard, Toast, getPreferenceValues, showHUD, showInFinder, showToast
 import axios from "axios";
 import { getNowTime } from "./utils";
 import { join } from "path";
-import fs from "fs";
+import ffmpeg from "fluent-ffmpeg";
+
 export default async () => {
   let selectedText: string | undefined;
   try {
@@ -31,11 +32,26 @@ export default async () => {
   });
 
   const filename = `${getNowTime()}.${format}`;
-  await fs.promises.writeFile(join(directory, filename), data.data);
+  //await fs.promises.writeFile(join(directory, filename), data.data);
 
-  await showToast(Toast.Style.Success, "合成成功");
-  await showHUD("合成成功");
-  if (open) {
-    await showInFinder(join(directory, filename));
-  }
+  const command = ffmpeg(data.data);
+
+  command
+    .toFormat(format)
+    .audioBitrate("192k")
+    .audioChannels(2)
+    .audioFrequency(16000)
+    .on("error", async (err) => {
+      console.log("An error occurred: " + err.message);
+      await showToast(Toast.Style.Failure, "合成失败");
+      await showHUD("合成失败");
+    })
+    .on("end", async () => {
+      await showToast(Toast.Style.Success, "合成成功");
+      await showHUD("合成成功");
+      if (open) {
+        await showInFinder(join(directory, filename));
+      }
+    })
+    .save(join(directory, filename));
 };
